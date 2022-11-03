88d0e27852bd48908b82e140e74de5ce3af09b3b
baihaqi2193 <baihaqi2193@gmail.com>
2022-11-03 12:13:53 +0700

    Added add and remove to-do feature from the list

--- src/ToDoListApp.java
+++ src/ToDoListApp.java
@@ -2,6 +2,9 @@ public class ToDoListApp {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
+
+        testAddToDoList();
+        testRemoveToDoList();
         testShowToDoList();
     }
     // Business Logic
@@ -10,9 +13,9 @@ public class ToDoListApp {
      * showToDoList
      * TODO : Menampilkan to-do-list
      */
-    public static void showToDoList(String[] data){
-        for (var i = 0; i < data.length ; i++ ) {
-            String todo = data[i];
+    public static void showToDoList(){
+        for (var i = 0; i < model.length ; i++ ) {
+            String todo = model[i];
             if(todo != null) {
                 System.out.println("[" + (i+1) + "]" + " " + todo);
             }
@@ -20,23 +23,88 @@ public class ToDoListApp {
     }
 
     public static void testShowToDoList(){
-        model[0] = "Berdoa";
-        model[1] = "Gosok gigi";
-        model[2] = "Wudhu";
-        showToDoList(model);
+        showToDoList();
     }
 
     /**
      * addToDoList
      * TODO : Menambahkan to-do ke list
      */
-    public static  void addToDoList(){}
+    public static void addToDoList(String todo){
+        // Cek apakah di model sudah penuh
+        var isFull = true;
+        for (int i = 0; i < model.length; i++) {
+            // Jika ada yang null berarti tidak penuh
+            if(model[i] == null) {
+                // Set flag to false
+                isFull = false;
+                break;
+            }
+        }
+
+        if(isFull) {
+            var tempModel = model;
+            model = new String[model.length * 2];
+            for (int i = 0; i < tempModel.length; i++) {
+                model[i] = tempModel[i];
+            }
+        }
+
+        // Cek dimana data null (kosong)
+        for (int i = 0; i < model.length; i++) {
+            // Dapatkan indeks dimana data null
+            if (model[i] == null) {
+                // Add to-do ke indeks data null
+                model[i] = todo;
+                break;
+            }
+        }
+
+
+    }
+
+    public static void testAddToDoList(){
+        for (int i = 1; i <= 5; i++) {
+            addToDoList("Todo ke-" + i);
+        }
+    }
 
     /**
      * removeToDoList
      * TODO : Menghapus to-do dari list
      */
-    public static  void removeToDoList(){}
+    public static boolean removeToDoList(Integer idx){
+        var realIdx = idx - 1;
+        // Kalau lebih dari index ga valid
+        if(realIdx >= model.length){
+            return false;
+        } else if (realIdx <= 0) {
+            return false;
+        }
+
+        // Kalau udah null, ga valid
+        if (model[realIdx] == null) {
+            return false;
+        } else {
+            // Geser kalau nggak null
+            for (int i = realIdx; i < model.length ; i++) {
+                // Kalau di index akhir langsung jadi null
+                if(i == model.length - 1){
+                    model[i] = null;
+                }
+                // Kalau nggak di akhir, geser aja langsung
+                else {
+                    model[i] = model[i + 1];
+                }
+
+            }
+            return true;
+        }
+    }
+
+    public static void testRemoveToDoList(){
+        System.out.println(removeToDoList(0));
+    }
 
     // View
     /**
